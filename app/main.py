85534f1da6b39d53d3b66b12694dbfda5bef9ddf
Beker-Dev <joaopedro.beker@hotmail.com>
2023-04-01 19:14:37 -0300

    feat: added jwt class

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
-from app.generators.secrets import secret_key_generator
-from app.generators.jwt import jwt_token_generator, jwt_token_validator
+from app.generators.jwt import JWT
 
 
 if __name__ == "__main__":
     payload = {'user_id': 1, 'username': 'admin'}
-    tkn = jwt_token_generator(payload=payload)
-    print(jwt_token_validator(tkn))
+    jwt = JWT()
+    tkn = jwt.get_token(payload)
