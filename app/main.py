74c6ab26e0c9ff2ab45c1c508b6520620ece1ff6
Beker-Dev <joaopedro.beker@hotmail.com>
2023-04-01 17:14:28 -0300

    feat: added secret_key generator function

--- app/main.py
+++ app/main.py
@@ -1,26 +1,5 @@
-from app.decorators import handle_exceptions, return_type_validator
-
-
-class Test:
-    def __init__(self):
-        self.x = 2
-        self.y = 0
-
-    def __str__(self):
-        return self.__class__.__name__
-
-    @handle_exceptions(TypeError, ValueError)
-    def raise_test_exception(self, *args):
-        raise ValueError("Test Exception - args: ", args)
-
-    @return_type_validator(str)
-    def getn(self, n):
-        return 'n + 30'
-
-
-test = Test()
+from app.generators import secret_key_generator
 
 
 if __name__ == "__main__":
-    print(test.raise_test_exception('test', 'test2', 'test3', []))
-    print(test.getn(1))
+    print(secret_key_generator())
