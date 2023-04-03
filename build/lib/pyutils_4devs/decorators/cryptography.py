46674dca097967b318799fc066ce1ec0a1e23f1e
Beker-Dev <joaopedro.beker@hotmail.com>
2023-04-03 16:49:53 -0300

    update setup

--- build/lib/pyutils_4devs/decorators/cryptography.py
+++
@@ -1,7 +0,0 @@
-def fernet_key_validator(func):
-    def decorator(self, *args, **kwargs):
-        if self.fernet_key is None:
-            raise ValueError("Fernet key is not set, call method set_fernet_key() and save it")
-        else:
-            return func(self, *args, *kwargs)
-    return decorator
